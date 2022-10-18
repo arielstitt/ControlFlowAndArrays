9a4f5bcac4f43f177a7aa4f35d99c80f295801d7
Ariel Stitt <astitt@perscholas.org>
2022-10-18 17:45:17 -0400

    created superclasses and a factory

--- exercises/objetctsAndOrgc/orcs.js
+++ exercises/objetctsAndOrgc/orcs.js
@@ -101,9 +101,8 @@ const adventurer = {
 //   return newEnemy;
 // };
 
-
 // const createPlayer = (nameIs, healthIs, powerIs, staminaIs) => {
-  
+
 //     const newPlayer = {
 //     name: nameIs,
 //     health: healthIs,
@@ -114,32 +113,129 @@ const adventurer = {
 //   return newPlayer;
 // };
 
-class Character {
-    //constructor
-    constructor (name, age, eyes, hair, lovesCats = false, lovesDogs) {
-        this.legs = 2;
-        this.arms = 2;
-        this.name = name;
-        this.age = age;
-        this.eyes = eyes;
-        this.hair = hair;
-        this.lovesCats = lovesCats;
-        this.lovesDogs = lovesDogs;
-    }
-    greet(otherCharacter) {
-        console.log('hi ' + otherCharacter + "!" );
-    }
-    smite() {
-        console.log('I smite thee you vile person');
-    }
+// class Character {
+//   //constructor
+//   constructor(name, age, eyes, hair, lovesCats = false, lovesDogs) {
+//     this.legs = 2;
+//     this.arms = 2;
+//     this.name = name;
+//     this.age = age;
+//     this.eyes = eyes;
+//     this.hair = hair;
+//     this.lovesCats = lovesCats;
+//     this.lovesDogs = lovesDogs || false;
+//   }
+//   //setter method
+//   setHair(hairColor) {
+//     this.hair = hairColor;
+//   }
+//   greet(otherCharacter) {
+//     console.log("hi " + otherCharacter + "!");
+//   }
+//   classyGreeting(otherClassyCharacter) {
+//     console.log("Greetings " + otherClassyCharacter.name + "!");
+//   }
+//   smite() {
+//     console.log("I smite thee you vile person");
+//   }
+// }
+
+// const p1 = new Character("Ariel", 97, "green", "silver", true, true);
+// const enemy = new Character("Sauron", 10000, "red");
+
+// // console.log(p1.greet("bob"));
+// // console.log(p1.smite());
+
+// // console.log(enemy.greet("sauron"));
+
+// console.log(enemy.classyGreeting(p1));
+
+//PARENT CLASSES
+
+// class Character {
+//   constructor(name, age, eyes, hair, lovesCats = true, lovesDogs) {
+//     this.legs = 2;
+//     this.arms = 2;
+//     this.name = name;
+//     this.age = age;
+//     this.eyes = eyes;
+//     this.hair = hair;
+//     this.lovesCats = lovesCats;
+//     this.lovesDogs = lovesDogs || true;
+//   }
+//   greet(otherCharacter) {
+//     console.log("hi " + otherCharacter + "!");
+//   }
+//   classyGreeting(otherClassyCharacter) {
+//     console.log("Howdy " + otherClassyCharacter.name + "!");
+//   }
+//   setHair(hairColor) {
+//     this.hair = hairColor;
+//   }
+//   smite() {
+//     console.log("i smited thee.");
+//   }
+// }
+
+// class Hobbit extends  Character {
+//     constructor(name, age, eyes, hair) {
+//         super(name, age, eyes, hair);
+//         this.skills = ["thievery", "speed", "willpower"];
+//     }
+//     steal () {
+//         console.log("let's get away!")
+//     }
+//     greet (otherCharacter) {
+//         console.log(`Hobbit says hello to ${otherCharacter}`)
+//     }
+//     smite() {
+//         super.smite();
+//         this.steal();
+//     }
+// }
+
+// const frodo = new Hobbit("Mr Baggins", 33, "blue", "brown");
+// // console.log(frodo);
+// // console.log(frodo.steal());
+// // console.log(frodo.greet("Sam"));
+
+// console.log(frodo);
+
+class Car {
+  constructor(maker, serialNumber) {
+    this.serialNumber = serialNumber;
+    this.maker = maker;
+  }
+  drive() {
+    console.log("vroom vroom");
+  }
 }
 
-const p1 = new Character("Ariel", 97, "green", "silver", true, true);
-const enemy = new Character();
+//factory class
+class Factory {
+  constructor(company) {
+    this.company = company;
+    this.cars = [];
+  }
+  generateCar() {
+    const newCar = new Car(this.company, this.cars.length);
+    this.cars.push(newCar);
+  }
+  findCar (index) {
+    return this.cars[index]
+  }
+}
 
-console.log(p1.greet("bob"));
-console.log(p1.smite());
+const tesla = new Factory('Tesla');
+tesla.generateCar();
+tesla.generateCar();
+tesla.generateCar();
+console.log(tesla);
+console.log(tesla.findCar(0))
 
-console.log(enemy.greet("sauron"));
-
-console.log(p1);
+const porche = new Factory('Porche');
+porche.generateCar();
+porche.generateCar();
+porche.generateCar();
+console.log(porche);
+console.log(porche.findCar(1));
